e340d6eb95bd3b4251fa02e8d937e006f7011246
misab <misabajandinov@gmail.com>
2025-01-27 02:43:52 +0200

    Create CarServices(Create)

--- CarTARgv23/CarTARgv23/ApplicationServices/CarServices.cs
+++ CarTARgv23/CarTARgv23/ApplicationServices/CarServices.cs
@@ -1,9 +1,48 @@
-﻿using CarTARgv23.Core.ServerInterface;
+﻿using CarTARgv23.Core.Domain;
+using CarTARgv23.Core.ServerInterface;
+using CarTARgv23.Data;
 
 namespace CarTARgv23.ApplicationServices
 {
     public class CarServices : ICarServices
     {
+        private readonly CarContext _context;
         
+
+
+        public KindergartenServices(CarContext context)
+        {
+            _context = context;
+            
+        }
+
+        public async Task<Car> DetailsAsync(Guid id)
+        {
+            var result = await _context.Cars
+                .FirstOrDefaultAsync(x => x.Id == id);
+
+            return result;
+        }
+
+        public async Task<Car> Create(CarDto dto)
+        {
+            Car kindergarten = new Car
+            {
+                Id = Guid.NewGuid(),
+                Brand = dto.Brand,
+                Model = dto.Model,
+                Number = dto.Number,
+                Mileage = dto.Mileage,
+                TechnicalInspection = dto.TechnicalInspection,
+                CreatedAt = DateTime.Now,
+                UpdatedAt = DateTime.Now
+            };
+
+            await _context.Kindergartens.AddAsync(kindergarten);
+            await _context.SaveChangesAsync();
+
+            return kindergarten;
+        }
+
     }
 }
