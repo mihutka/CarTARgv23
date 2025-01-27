22301b57f4c646722a221ea01e69426cd8cb1d33
misab <misabajandinov@gmail.com>
2025-01-27 02:52:19 +0200

    Update ICarService, update CarService

--- CarTARgv23/CarTARgv23/ApplicationServices/CarServices.cs
+++ CarTARgv23/CarTARgv23/ApplicationServices/CarServices.cs
@@ -1,19 +1,21 @@
-﻿using CarTARgv23.Core.Domain;
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CarTARgv23.Core.Domain;
 using CarTARgv23.Core.ServerInterface;
 using CarTARgv23.Data;
+using CarTARgv23.Core.Dto;
 
 namespace CarTARgv23.ApplicationServices
 {
     public class CarServices : ICarServices
     {
         private readonly CarContext _context;
-        
 
-
-        public KindergartenServices(CarContext context)
+        public CarServices(CarContext context)
         {
             _context = context;
-            
         }
 
         public async Task<Car> DetailsAsync(Guid id)
@@ -26,7 +28,7 @@ namespace CarTARgv23.ApplicationServices
 
         public async Task<Car> Create(CarDto dto)
         {
-            Car kindergarten = new Car
+            var car = new Car
             {
                 Id = Guid.NewGuid(),
                 Brand = dto.Brand,
@@ -38,11 +40,47 @@ namespace CarTARgv23.ApplicationServices
                 UpdatedAt = DateTime.Now
             };
 
-            await _context.Kindergartens.AddAsync(kindergarten);
+            await _context.Cars.AddAsync(car);
             await _context.SaveChangesAsync();
 
-            return kindergarten;
+            return car;
         }
 
+        public async Task<Car> Update(CarDto dto)
+        {
+            var car = await _context.Cars.FirstOrDefaultAsync(x => x.Id == dto.Id);
+            if (car == null)
+            {
+                return null;
+            }
+
+            car.Brand = dto.Brand;
+            car.Model = dto.Model;
+            car.Number = dto.Number;
+            car.Mileage = dto.Mileage;
+            car.TechnicalInspection = dto.TechnicalInspection;
+            car.UpdatedAt = DateTime.Now;
+
+            _context.Cars.Update(car);
+            await _context.SaveChangesAsync();
+
+            return car;
+        }
+
+        public async Task<Car> Delete(Guid id)
+        {
+            var car = await _context.Cars
+                .FirstOrDefaultAsync(x => x.Id == id);
+
+            if (car == null)
+            {
+                return null;
+            }
+
+            _context.Cars.Remove(car);
+            await _context.SaveChangesAsync();
+
+            return car;
+        }
     }
 }
--- CarTARgv23/CarTARgv23/Core/ServerInterface/ICarServices.cs
+++ CarTARgv23/CarTARgv23/Core/ServerInterface/ICarServices.cs
@@ -1,7 +1,18 @@
-﻿namespace CarTARgv23.Core.ServerInterface
+﻿using CarTARgv23.Core.Domain;
+using CarTARgv23.Core.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarTARgv23.Core.ServerInterface
 {
     public interface ICarServices
     {
-
+        Task<Car> DetailsAsync(Guid id);
+        Task<Car> Update(CarDto dto);
+        Task<Car> Delete(Guid id);
+        Task<Car> Create(CarDto dto);
     }
 }
